446735ab10b082654cee21946c1e1f2a2b0f2cd4
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2021-10-24 20:14:49 +0200

    Nowy projekt C#

---
+++ Zestaw 2 - Zadanie B/Zadanie_B.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AwokeKnowing.GnuplotCSharp;
+
+namespace Zadanie_B {
+    class Program {
+        static void Main(string[] args) {
+            
+            GnuPlot.Plot("sin(x) + 2", "lc rgb \"magenta\" lw 5");
+            
+        }
+    }
+}
