1b7598f19fd1822ccfe91ebb0a0925d8bf0c9500
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2021-11-02 13:02:14 +0100

    Zadanie 2C znaczny postęp w zadaniu

--- Zestaw 2 - Zadanie C/Program.cs
+++ Zestaw 2 - Zadanie C/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using AwokeKnowing.GnuplotCSharp;
 
-namespace Zestaw_2___Zadanie_C
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Zadanie_2C {
+    class Program {
+        static void Main(string[] args) {
+            
+            int rozmiar_tablicy = 50;
+            
+            float[] wyniki_float = new float[rozmiar_tablicy];
+            double[] wyniki_double = new double[rozmiar_tablicy];
+            decimal[] wyniki_decimal = new decimal[rozmiar_tablicy];
+            
+            wyniki_decimal[0] = 1/3m;
+            wyniki_float[0] = 1/3f;
+            wyniki_double[0] = 1/3d;
+            
+            for (int i = 1; i < rozmiar_tablicy; i++) {
+                wyniki_decimal[i] = 4 * wyniki_decimal[i-1] - 1;
+                wyniki_float[i] = 4 * wyniki_float[i-1] - 1;
+                wyniki_double[i] = 4 * wyniki_double[i-1] - 1;
+            }
+            
+            GnuPlot.HoldOn();
+            GnuPlot.Plot(Array.ConvertAll(wyniki_decimal, x => (double)x), "with line");
+            GnuPlot.Plot(Array.ConvertAll(wyniki_float, x => (double)x), "with line");
+            GnuPlot.Plot(wyniki_double, "with line");
+            GnuPlot.HoldOff();
+            
         }
     }
 }
---
+++ Zestaw 2 - Zadanie C/README.md
@@ -0,0 +1,14 @@
+# Zestaw 2 - Zadanie C
+
+## Treść zadania
+
+Proszę zaimplementować przykład z wykładu profesora. Mamy zatem ciąg:
+
+$
+\begin{cases}
+x_{n+1} = 4x_{n} - 1 \\
+x_{0} = \frac{1}{3}
+\end{cases}
+$
+
+i badamy jego rozbieżność. Proszę w implementacji wykorzystać szeroki wachlarz reprezentacji liczb zmiennoprzecinkowych. Wyniki proszę zilustrować wykresem.
