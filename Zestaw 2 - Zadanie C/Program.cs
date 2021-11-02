6433ed5da124e574b3304c10918dc5b38e65927f
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2021-11-02 19:12:18 +0100

    Zadanie 2C Gotowe

--- Zestaw 2 - Zadanie C/Program.cs
+++ Zestaw 2 - Zadanie C/Program.cs
@@ -5,7 +5,7 @@ namespace Zadanie_2C {
     class Program {
         static void Main(string[] args) {
             
-            int rozmiar_tablicy = 50;
+            int rozmiar_tablicy = 95; // mniejszej liczby nie da się zapisać w zmiennej typu decimal
             
             float[] wyniki_float = new float[rozmiar_tablicy];
             double[] wyniki_double = new double[rozmiar_tablicy];
@@ -21,6 +21,7 @@ namespace Zadanie_2C {
                 wyniki_double[i] = 4 * wyniki_double[i-1] - 1;
             }
             
+            // konmwersja na tablice double jest wymagana, ponieważ biblioteka GnuPlot nie przyjmuje innych typów
             GnuPlot.Set ("multiplot layout 1,3");
             GnuPlot.Plot(Array.ConvertAll(wyniki_decimal, x => (double)x), "title \"decimal\" with line");
             GnuPlot.Plot(Array.ConvertAll(wyniki_float, x => (double)x), "title \"float\" with line");
